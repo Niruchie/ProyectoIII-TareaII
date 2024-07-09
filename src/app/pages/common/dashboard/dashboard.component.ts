88b644033553478f074cd090831699e15623935b
Niruchie <34629718+Niruchie@users.noreply.github.com>
2024-07-09 15:11:40 -0600

    feature(Home,Dashboard): Home and dashboard templates included.

--- src/app/pages/common/dashboard/dashboard.component.ts
+++ src/app/pages/common/dashboard/dashboard.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
+
+import { AuthenticationService } from '../../../service/authentication.service';
+import RoleEnum from '../../../../types/RoleEnum';
 
 @Component({
+  imports: [RouterLink,NgIf],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  protected canAccess = inject(AuthenticationService)
+    .hasRole(RoleEnum.SUPER_ADMIN_ROLE);
 }
